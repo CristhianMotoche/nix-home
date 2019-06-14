dd6ce4a68c280b4e23e4a429ffb4a3e9fc200e0b
CristhianMotoche <cristhian.motoche@gmail.com>
2019-06-13 23:21:26 -0500

    Add diff-so-fancy

--- home.nix
+++ home.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    gitAndTools.diff-so-fancy
     firefox
+    gcc
     stack
     slack
     xorg.xmodmap
     tmux
     neovim
+    (python27.withPackages(ps: with ps; [ pip setuptools neovim pynvim ]))
+    (python37.withPackages(ps: with ps; [ pip setuptools neovim pynvim ]))
+    xclip
   ];
 }
