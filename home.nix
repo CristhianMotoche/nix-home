53ff7cf438b42ede85328158cbe82bca60b69a8f
CristhianMotoche <cristhian.motoche@gmail.com>
2019-06-15 17:24:08 -0500

    Add steam

--- home.nix
+++ home.nix
@@ -4,12 +4,13 @@
     gitAndTools.diff-so-fancy
     firefox
     gcc
+    steam
     stack
     slack
     xorg.xmodmap
     tmux
     neovim
-    (python27.withPackages(ps: with ps; [ pip setuptools neovim pynvim ]))
+    (python27.withPackages(ps: with ps; [ pip setuptools neovim pynvim enum ]))
     (python37.withPackages(ps: with ps; [ pip setuptools neovim pynvim ]))
     xclip
   ];
