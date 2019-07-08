2312ab45e501d2a243e3fd5d5d4f4014aa57c29d
CristhianMotoche <cristhian.motoche@gmail.com>
2019-07-07 21:16:40 -0500

    Add some extra packages

--- home.nix
+++ home.nix
@@ -1,17 +1,27 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    bluez
+    ctags
+    direnv
     gitAndTools.diff-so-fancy
     firefox
+    libGL
+    libstdcxx5
     gcc
+    glxinfo
     steam
     stack
     slack
     xorg.xmodmap
     tmux
+    tmuxinator
+    pavucontrol
     neovim
     (python27.withPackages(ps: with ps; [ pip setuptools neovim pynvim enum ]))
     (python37.withPackages(ps: with ps; [ pip setuptools neovim pynvim ]))
+    vlc
     xclip
+    unzip
   ];
 }
